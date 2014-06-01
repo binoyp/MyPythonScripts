445c815e7549c3cb316c169b569e0705aecf7901
Binoy Pilakkat <binoypilakkat@hotmail.com>
2014-06-01 20:23:37 +0530

    Comments added

--- raw2jpeg.py
+++ raw2jpeg.py
@@ -10,13 +10,21 @@ import os
 
 import xml.etree.ElementTree as ET
 def genSettingsfiles():
-    _fl = os.listdir(r'F:\DCIM\100CANON')
-    xm = open(r'K:\Pics\test.ufraw','r')
+    """
+    Generates ufraw id files for conversion of raw images to jpeg using ufraw
+    specify the input folder containing raw images
+    specify a ufraw id template. Using this template new id files are generated
+    for conversion using ufraw batch command
+    Script executes the ufraw batch command using the newly generated id files.
+
+    """
+    _fl = os.listdir(r'F:\DCIM\100CANON') #Input folder with raw images
+    xm = open(r'K:\Pics\test.ufraw','r')    # Sample ufraw id file as template
     data = xm.read()
-    
+
     xm.close()
-    
-    
+
+
     tree = ET.parse(r'K:\Pics\test.ufraw')
     root = tree.getroot()
     for e in _fl:
@@ -24,17 +32,18 @@ def genSettingsfiles():
             inpf = os.path.join(r'F:\DCIM\100CANON',e)
             of = e[:-4]+'.jpg'
             ur = e[:-4]+'.ufraw'
-            outf= os.path.join(r'K:\Pics',of)
-            urf = os.path.join(r'K:\Pics\ufraws',ur)
+            outf= os.path.join(r'K:\Pics',of) #Output Folder path
+            urf = os.path.join(r'K:\Pics\ufraws',ur) #Generated ufraw id files
+                                                        #path
             print outf
 cmd = '"C:\\Program Files (x86)\\UFRaw\\bin\\ufraw-batch.exe" --conf= '
 for f in os.listdir(r'K:\Pics\ufraws'):
     exc = cmd + os.path.join(r'K:\\Pics\\ufraws',f)
-    print exc  
-    run = os.system(exc)    
+    print exc
+    run = os.system(exc)
     print run
     if run:
-        break            
+        break
 #        for child in root.iter('OutputFilename'):
 #          child.text = outf
 #        for child in root.iter('InputFilename'):
